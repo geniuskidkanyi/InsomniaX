5df9f18d4262c3436c49e06af4be32be456db508
muhammed kanyi <kanyi@gcubed.gm>
2015-10-28 16:24:05 +0000

    {{

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   get 'users/new'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,6 +8,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#index'
+#  get 'help_path' => 'pages/help'
+  resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
