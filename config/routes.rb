6b2ec1a11a72f7f11964df1dd51008f96b79bbc1
muhammed kanyi <kanyi@gcubed.gm>
2015-10-29 13:45:44 +0000

    added session cotroller wither render new

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-
-
-  get 'users/new'
+  root 'pages#index'
+  get 'help'  => 'pages#help'
+  get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
+  get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#index'
+
 #  get 'help_path' => 'pages/help'
   resources :users
 
