6de76892df342cbf4fce54e8b069b28b7f26b065
muhammed kanyi <kanyi@gcubed.gm>
2015-11-17 15:55:20 +0000

    added user index page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,10 @@ module SessionsHelper
         cookies.permanent.signed[:user_id] = user.id
         cookies.permanent[:remember_token] = user.remember_token
     end
+    # Returns true if the given user is the current user.
+    def current_user?(user)
+        user == current_user
+    end
     # Returns the user corresponding to the remember token cookie.
     def current_user
         if (user_id = session[:user_id])
@@ -29,8 +33,23 @@ module SessionsHelper
     def logged_in?
         !current_user.nil?
     end
+    # Forgets a persistent session.
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
+    end
   def log_out
     session.delete(:user_id)
     @current_user = nil
   end
+    # Redirects to stored location (or to the default).
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+    # Stores the URL trying to be accessed.
+    def store_location
+        session[:forwarding_url] = request.url if request.get?
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   format: { with: VALID_EMAIL_REGEX },
   uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+    validates :password, length: { minimum: 6 }, allow_blank: true
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
       BCrypt::Engine.cost
@@ -26,4 +26,8 @@ class User < ActiveRecord::Base
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
+    # Forgets a user.
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
 end
