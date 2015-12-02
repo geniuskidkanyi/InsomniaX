18cb0e09231a79996aa5f96b402db53d2f69d4e7
muhammed kanyi <kanyi@gcubed.gm>
2015-12-02 09:15:56 +0000

    fixed the follers count

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
     has_many :microposts, dependent: :destroy
     has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
     has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-    has_many :following, through: :active_relationships, source: :followed
+    has_many :following, through: :passive_relationships, source: :followed
     has_many :followers, through: :active_relationships, source: :follower
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
