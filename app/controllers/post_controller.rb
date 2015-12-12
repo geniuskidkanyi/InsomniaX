87145c412cf871918388e873780a95f0778d4768
muhammed kanyi <kanyi@gcubed.gm>
2015-12-12 10:37:05 +0000

    moving to main forum

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,8 @@
+class PostController < ApplicationController
+  def index
+  end
+  def show
+  end
+  def create
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151211231757) do
+ActiveRecord::Schema.define(version: 20151212100717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -134,6 +134,13 @@ ActiveRecord::Schema.define(version: 20151211231757) do
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at", using: :btree
   add_index "microposts", ["user_id"], name: "index_microposts_on_user_id", using: :btree
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
