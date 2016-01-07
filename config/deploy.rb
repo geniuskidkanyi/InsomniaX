f99b57b6abbd02d2b1d05de2728c6bc1201f647e
muhammed kanyi <kanyi@gcubed.gm>
2016-01-07 09:05:32 +0000

    added asset precompilation

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,6 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
@@ -100,6 +99,7 @@ namespace :deploy do
     end
 
     before :starting,     :check_revision
+    after  :finishing,    :compile_assets
     after  :finishing,    :cleanup
     after  :finishing,    :restart
 end
