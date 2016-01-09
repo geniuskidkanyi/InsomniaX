b8eb2057b749b288d532ba827eb0244a05b9dc1b
muhammed kanyi <kanyi@gcubed.gm>
2016-01-09 12:14:52 +0000

    removed fixed navigation for mobile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -63,27 +63,21 @@ $(document).ready(function () {
     );
 });
 
-function init() {
-
-    var _inputs = document.getElementsByTagName('input');
-
-    for (var i = 0; i < _inputs.length; i++) {
-
-        if (!Modernizr.inputtypes[_inputs[i].type]) {
-            _inputs[i].className = 'not-supported';
+var supports = (function () {
+    var d = document.documentElement,
+        c = "ontouchstart" in window || navigator.msMaxTouchPoints;
+    if (c) {
+        d.className += " touch";
+        return {
+            touch: true
         }
-
-        _inputs[i].parentNode.getElementsByClassName('result')[0].innerHTML = _inputs[i].value;
-
-        _inputs[i].onchange = function () {
-            //console.log(this.value);
-            var result_node = this.parentNode.getElementsByClassName('result');
-            result_node[0].innerHTML = this.value;
+    } else {
+        d.className += " no-touch";
+        return {
+            touch: false
         }
     }
-}
-
-window.onload = init;
+})();
 
 $('.vote')
     .on('ajax:send', function () {
