3cbf7b5a482a3cfa81e59343e699eafa7d31a87d
muhammed kanyi <kanyi@gcubed.gm>
2016-01-13 10:40:54 +0000

    removed the merge conflict

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,12 +3,8 @@ class Article < ActiveRecord::Base
     extend FriendlyId
     friendly_id :title, use: :slugged
     is_impressionable
-<<<<<<< HEAD
-
-=======
     mount_uploader :picture_article, PictureArticleUploader
     acts_as_commentable
->>>>>>> blog
     belongs_to :user
     has_many :comments
     acts_as_taggable
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160109101520) do
-=======
+
 ActiveRecord::Schema.define(version: 20160113101325) do
->>>>>>> blog
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
