725c4bc69442aaa2566477e3f36ba5f57fc77c3b
muhammed kanyi <kanyi@gcubed.gm>
2016-01-14 13:23:19 +0000

    added some helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -34,13 +34,7 @@ module UsersHelper
         avatar_url = user.avatar? ? user.avatar.url : gravatar_url
         image_tag(avatar_url, alt: user.name, class: "img-circle profile-avatar")
     end
- def gravatar_post(user, options = { size: 80 })
-        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-        size = options[:size]
-        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-     avatar_url = user.avatar? ? user.avatar.url : gravatar_url
-     image_tag(avatar_url, alt: user.name, class: "img-circle avatar")
-    end
+
 
     def gravatar_message(user, options = { size: 80 })
         gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
@@ -48,6 +42,13 @@ module UsersHelper
         gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
         avatar_url = user.avatar? ? user.avatar.url : gravatar_url
         image_tag(avatar_url, alt: user.name, class: "message-avatar")
+    end
+    def gravatar_post(user, options = { size: 80 })
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        size = options[:size]
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        avatar_url = user.avatar? ? user.avatar.url : gravatar_url
+        image_tag(avatar_url, alt: user.name, class: "img-circle avatar")
     end
     def gravatar_forum(user, options = { size: 80 })
         gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
