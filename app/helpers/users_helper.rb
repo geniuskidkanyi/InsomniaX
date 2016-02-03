2d0276aa5bf92462a1426b0fb63170bd683364fc
muhammed kanyi <kanyi@gcubed.gm>
2016-02-03 08:27:59 -0500

    made fixed sidebar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -39,7 +39,7 @@ module UsersHelper
         size = options[:size]
         gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
      avatar_url = user.avatar? ? user.avatar.url : gravatar_url
-     image_tag(avatar_url, alt: user.name, class: "img-circle avatar")
+     image_tag(avatar_url, alt: user.name, class: "circle")
     end
 
     def gravatar_message(user, options = { size: 80 })
