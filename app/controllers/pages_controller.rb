df7752a5504d061fa1460d93f63046016d601a84
muhammed kanyi <kanyi@gcubed.gm>
2016-02-25 23:25:46 -0500

    added feed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
     def index
         if logged_in?
             @micropost = current_user.microposts.build
+            @feed_items = current_user.feed.paginate(page: params[:page])
          @activities = PublicActivity::Activity.order("created_at desc").limit(20)
         end
     end
