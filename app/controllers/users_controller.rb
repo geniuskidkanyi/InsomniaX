6142bb38b4e49f0aed24865d8bcc05b2cac1ecca
muhammed kanyi <kanyi@gcubed.gm>
2016-03-28 16:30:55 +0000

    adding load more

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-    before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
+   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
     before_action :correct_user, only: [:edit, :update]
     before_action :admin_user, only: :destroy
   def new
    @user = User.new
   end
  def index
-     
+
      @users = User.paginate(page: params[:page])
 
  end
@@ -78,4 +78,3 @@ class UsersController < ApplicationController
     end
 
 end
-
