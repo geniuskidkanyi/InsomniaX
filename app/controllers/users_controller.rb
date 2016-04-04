8104eb446b975c6c2cb289ee368227145b32eb69
muhammed kanyi <kanyi@gcubed.gm>
2016-04-04 06:58:08 +0000

    styling pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,19 +6,7 @@ class UsersController < ApplicationController
    @user = User.new
   end
  def index
-
-   # if the id params is present
-       if params[:id]
-         # get all records with id less than 'our last id'
-         # and limit the results to 5
-         @users = User.where('id > ?', params[:id]).limit(5)
-       else
-         @users = User.limit(5)
-       end
-       respond_to do |format|
-         format.html
-         format.js
-       end
+   @users = User.paginate(page: params[:page])
 
 end
 
