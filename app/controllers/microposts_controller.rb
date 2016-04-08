3c2418a0691ea93eeaf234d88a15b463561fe6f1
muhammed kanyi <kanyi@gcubed.gm>
2016-04-08 11:33:48 +0000

    beautify evrerything

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,46 +1,50 @@
 class MicropostsController < ApplicationController
-    before_action :logged_in_user, only: [:create, :destroy]
-    before_action :correct_user,
-    only: :destroy
-    def create
-        @micropost = current_user.microposts.build(micropost_params)
-        if @micropost.save
-            flash[:success] = "Micropost created!"
-            redirect_to root_url
-        else
-            @feed_items = []
-            render 'pages/index'
-        end
+  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,
+                only: :destroy
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = 'Micropost created!'
+      redirect_to root_url
+    else
+      @feed_items = []
+      render 'pages/index'
     end
-    def destroy
-        @micropost.destroy
-        flash[:success] = "Micropost deleted"
-        redirect_to request.referrer || root_url
-    end
-    def upvote
-        @micropost = Micropost.find(params[:id])
-        @micropost.liked_by current_user
-        respond_to do |format|
-            format.html {redirect_to :back }
-            format.json { render json: { count: micropost.get_upvote.size } }
-        end
+  end
 
-    end
+  def destroy
+    @micropost.destroy
+    flash[:success] = 'Micropost deleted'
+    redirect_to request.referrer || root_url
+  end
 
-    def downvote
-        @micropost = Micropost.find(params[:id])
-        @micropost.downvote_from current_user
-        respond_to do |format|
-            format.html {redirect_to :back }
-            format.json { render json: { count: micropost.get_upvote.size } }
-        end
+  def upvote
+    @micropost = Micropost.find(params[:id])
+    @micropost.liked_by current_user
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.json { render json: { count: micropost.get_upvote.size } }
     end
-    private
-    def micropost_params
-        params.require(:micropost).permit(:content, :picture)
+  end
+
+  def downvote
+    @micropost = Micropost.find(params[:id])
+    @micropost.downvote_from current_user
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.json { render json: { count: micropost.get_upvote.size } }
     end
-    def correct_user
-        @micropost = current_user.microposts.find_by(id: params[:id])
-        redirect_to root_url if @micropost.nil?
-    end
+  end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:content, :picture)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
+  end
 end
