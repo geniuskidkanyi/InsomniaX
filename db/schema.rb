e6cb28387dc39b7a769072ca06eac9f16e4483a5
geniuskid kanyi <geniuskid@debian>
2016-06-24 15:36:09 +0000

    updated the font

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160515162418) do
+ActiveRecord::Schema.define(version: 20160406223358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -371,9 +371,6 @@ ActiveRecord::Schema.define(version: 20160515162418) do
     t.integer  "sash_id"
     t.integer  "level",                default: 0
     t.string   "dprofile"
-    t.string   "activation_digest"
-    t.boolean  "activated",            default: false
-    t.datetime "activated_at"
   end
 
   create_table "votes", force: :cascade do |t|
