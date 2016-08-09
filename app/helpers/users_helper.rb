f0c14c4111bedf4da3275f383c79a0cba2908f18
muhammed kanyi <kanyi@gcubed.gm>
2016-08-09 11:31:19 +0000

    change the sidenav

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -34,6 +34,20 @@ module UsersHelper
         dprofile_url = user.dprofile? ? user.dprofile.url : gravatar_url
         image_tag(dprofile_url, alt: user.name, class: " twPc-bg twPc-block")
     end
+    def gravatar_mprofile(user, options = { size: 80 })
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        size = options[:size]
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        avatar_url = user.avatar? ? user.avatar.url : gravatar_url
+        image_tag(avatar_url, alt: user.name, class: " circle")
+    end
+    def gravatar_mdprofile(user, options = { size: 80 })
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        size = options[:size]
+        gravatar_url = "http://bootdey.com/img/Content/bg_element.jpg"
+        dprofile_url = user.dprofile? ? user.dprofile.url : gravatar_url
+        image_tag(dprofile_url, alt: user.name, class: " background")
+    end
     def gravatar_comment(user, options = { size: 80 })
         gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
         size = options[:size]
