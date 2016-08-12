1603c4287190075f92b6013505267aa7da87f0d8
muhammed kanyi <kanyi@gcubed.gm>
2016-08-12 10:33:34 +0000

    remove padding from the body

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
+  Rails.application.config.action_cable.disable_request_forgery_protection = true
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
