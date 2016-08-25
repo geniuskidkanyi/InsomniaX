a96ec0140a7a17c54d0cdab2994373d28670bc53
muhammed kanyi <kanyi@gcubed.gm>
2016-08-25 15:27:14 +0000

    removing ckeditor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.assets.precompile += Ckeditor.assets
-  
+
+
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
