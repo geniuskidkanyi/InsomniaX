2edc8308375f6f6a074309640672c73971763dba
geniuskid kanyi <kanyi@gcubed.gm>
2017-01-04 08:46:39 +0000

    updated the artcilde

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,7 +47,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  helper_method :resource_name, :resource, :devise_mapping
 
+    def resource_name
+      :user
+    end
+
+    def resource
+      @resource ||= User.new
+    end
+
+    def devise_mapping
+      @devise_mapping ||= Devise.mappings[:user]
+    end
 protected
 def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username])
