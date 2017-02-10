0d6c273a079751d3e97c3f38a65885e19046403d
geniuskid kanyi <kanyi@gcubed.gm>
2017-02-10 23:11:33 +0000

    change the mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,16 @@ Rails.application.configure do
   config.action_cable.url = 'wss://insomniax.biz/cable'
   config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
   config.active_job.queue_adapter = :sucker_punch
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address   => "ocean.mxroute.com",
+      :port      => 465, # ports 587 and 2525 are also supported with STARTTLS
+      :enable_starttls_auto => true, # detects and uses STARTTLS
+      :user_name => "insomnia",
+      :password  => "S@eetbj1!", # SMTP password is any valid API key
+      :authentication => 'login', # Mandrill supports 'plain' or 'login'
+      :domain => 'insomniax.biz', # your domain to identify your server when connecting
+    }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply@insomniax.biz'}
